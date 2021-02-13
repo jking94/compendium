1e2acd2a1ab0ff4f3fbf7c6f6f3ea2b47d9556c7
David Bittner <david.r.bittner@gmail.com>
2021-02-12 19:00:12 -0500

    backtohome button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HomePageModule } from './views/home-page/home-page.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     SpellsPageModule,
--- src/app/views/spells-page/spells-page.module.ts
+++ src/app/views/spells-page/spells-page.module.ts
@@ -1,18 +1,21 @@
+import { HomeButtonComponent } from './../../components/home-button/home-button.component';
 import { RouterModule } from '@angular/router';
 import { SpellsPageComponent } from './spells-page.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { spellsPageRoutes } from './spells-page.routing';
-
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    SpellsPageComponent
+    SpellsPageComponent,
+    HomeButtonComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(spellsPageRoutes)
+    RouterModule.forChild(spellsPageRoutes),
+    MatButtonModule
   ]
 })
 export class SpellsPageModule { }
